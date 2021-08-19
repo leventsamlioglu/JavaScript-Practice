da5c3209e8c0d320907eb56163f2aff55af92348
leventsamlioglu <leventsamlioglu@icloud.com>
2021-08-19 17:21:13 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//Show stars on each line. We get 5 rows below,
+// in each row we have a star, and the number of
+// stars in that row, depends on the row we are in.
+
+showStars(5);
+
+function showStars(rows) {
+  let array = [];
+  for (let i = 1; i <= rows; i++) {
+    array += "*";
+    console.log(array);
+  }
+}
