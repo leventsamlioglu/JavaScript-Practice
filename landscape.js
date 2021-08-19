4adfe7fa9c841e0cf95aeb866e87c74e45f64ee2
leventsamlioglu <leventsamlioglu@icloud.com>
2021-08-19 17:42:04 +0200

    Function basics

---
+++ landscape.js
@@ -0,0 +1,9 @@
+// Function isLandscape, it takes two parameters,
+// width, and height of an image, and returns true
+// if the image is landscape
+
+console.log(isLandscape(800, 700));
+
+function isLandscape(width, height) {
+  return width > height;
+}
