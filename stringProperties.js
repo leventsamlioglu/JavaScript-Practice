18bf97f8f6ed6aef0a7d7efa0355362c677502aa
leventsamlioglu <leventsamlioglu@icloud.com>
2021-08-20 16:17:38 +0200

    Function basics

---
+++ stringProperties.js
@@ -0,0 +1,16 @@
+// Display all the properties of 'movie' object that are of type string
+
+const movie = {
+  title: "a",
+  releaseYear: 2018,
+  rating: 4.5,
+  director: "b",
+};
+
+showProperties(movie);
+
+function showProperties(obj) {
+  for (let key in obj) {
+    if (typeof obj[key] === "string") console.log(key, obj[key]);
+  }
+}
