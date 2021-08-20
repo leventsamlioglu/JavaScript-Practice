c9d78e3e9569fecf8c7c2bfa994f6622e15985ef
leventsamlioglu <leventsamlioglu@icloud.com>
2021-08-20 15:47:10 +0200

    Object constructor basics

--- landscape.js
+++
@@ -1,9 +0,0 @@
-// Function isLandscape, it takes two parameters,
-// width, and height of an image, and returns true
-// if the image is landscape
-
-console.log(isLandscape(800, 700));
-
-function isLandscape(width, height) {
-  return width > height;
-}
