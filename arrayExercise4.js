9e602fe397eddcbcb3eea3b2c8ad6a6780767ca6
leventsamlioglu <leventsamlioglu@icloud.com>
2021-08-25 16:57:47 +0200

    Array exercise

--- arrayExercise4.js
+++ arrayExercise4.js
@@ -1,9 +1,14 @@
-const numbers = [1, 2, 3, 1, 4, 1];
-
-const count = countOccurences(numbers, 1);
-console.log(count);
+try {
+  const numbers = [1, 2, 3, 1, 4, 1];
+  const count = countOccurences(numbers, 1);
+  console.log(count);
+} catch (e) {
+  console.log(e.message);
+}
 
 function countOccurences(array, searchElement) {
+  if (!Array.isArray(array)) throw new Error("Invalid array");
+
   return array.reduce((accumulator, current) => {
     const occurence = current === searchElement ? 1 : 0;
     return accumulator + occurence;
