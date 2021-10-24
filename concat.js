0ab00f7b7900fb79bb8a652ab7f113d7ee3b74dc
leventsamlioglu <leventsamlioglu@icloud.com>
2021-10-24 21:53:30 +0200

    add output comment

--- concat.js
+++ concat.js
@@ -1,3 +1,5 @@
-const arr=[1,[2,3],4,[5,6],7]
-const flat=[].concat(...arr)
-console.log(flat)
+const arr = [1, [2, 3], 4, [5, 6], 7];
+const flat = [].concat(...arr);
+console.log(flat);
+
+// Outputs:[1, 2, 3, 4, 5, 6, 7]
