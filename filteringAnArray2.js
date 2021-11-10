a1c64050c64e3520f30d677c8efbaac363c7e119
leventsamlioglu <leventsamlioglu@icloud.com>
2021-11-10 16:32:58 +0100

    modify code

--- filteringAnArray2.js
+++ filteringAnArray2.js
@@ -1,14 +1,9 @@
-const numbers = [1, 2, undefined, false, 3, NaN, "", "a", null, undefined, 0];
+function filterArray(array) {
+  return array.filter((x) => !!x || x === 0);
+}
 
-let filtered = numbers.filter((x) => !!x);
-// let filtered = numbers.filter((x) => x);
+console.log(
+  filterArray([1, 2, undefined, false, 3, NaN, "", "a", null, undefined, 0])
+);
 
-console.log(filtered);
-
-filtered = numbers.filter((x) => !!x || x === 0);
-
-console.log(filtered);
-
-// Outputs:
-// [ 1, 2, 3, 'a' ]
-// [ 1, 2, 3, 'a', 0 ]
+// Outputs: [ 1, 2, 3, 'a', 0 ]
