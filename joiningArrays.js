5b3a062b11151ca37e92d2bfec5126a8ecd31370
leventsamlioglu <leventsamlioglu@icloud.com>
2021-11-11 16:09:17 +0100

    add output comment

--- joiningArrays.js
+++ joiningArrays.js
@@ -1,10 +1,13 @@
 const numbers = [1, 2, 3, 4];
 const joined = numbers.join(",");
 console.log(typeof joined, joined);
+// string 1,2,3,4
 
 const message = "This is my first message";
 const parts = message.split(" ");
 console.log(typeof parts, parts);
+// object [ 'This', 'is', 'my', 'first', 'message' ]
 
 const combined = parts.join("-");
 console.log(combined);
+// This-is-my-first-message
