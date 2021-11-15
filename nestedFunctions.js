932a56b95d2293eb74621e6c7906c9a80f6f6f6e
leventsamlioglu <leventsamlioglu@icloud.com>
2021-11-15 09:28:13 +0100

    nested functions

--- nestedFunctions.js
+++ nestedFunctions.js
@@ -5,22 +5,32 @@ function outside(x) {
   return inside;
 }
 fn_inside = outside(3); // Think of it like: give me a function that adds 3 to whatever you give
-console.log("fn_inside", fn_inside);
 // it
 result = fn_inside(5); // returns 8
-console.log("result", result);
 
 result1 = outside(3)(5); // returns 8
-console.log("result1", result1);
 
-//------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------
 
 function addSquares(a, b) {
-  function square(x) {
-    return x * x;
+  function square(q) {
+    return q * q;
   }
   return square(a) + square(b);
 }
 a = addSquares(2, 3); // returns 13
 b = addSquares(3, 4); // returns 25
 c = addSquares(4, 5); // returns 41
+
+//----------------------------------------------------------------------------------------------
+
+function A(xx) {
+  function B(yy) {
+    function C(zz) {
+      console.log(xx + yy + zz);
+    }
+    C(3);
+  }
+  B(2);
+}
+A(1); // logs 6 (1 + 2 + 3)
