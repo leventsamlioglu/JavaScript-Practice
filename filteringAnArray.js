45a2d9be242a213e683a018192a0c97b754e32f2
leventsamlioglu <leventsamlioglu@icloud.com>
2021-11-18 15:02:51 +0100

    add output comment

--- filteringAnArray.js
+++ filteringAnArray.js
@@ -3,5 +3,5 @@ const numbers = [1, -1, 2, 3];
 const filteredValue = numbers.filter((value) => value >= 0);
 const filteredIndex = numbers.filter((_, index) => index > 1);
 
-console.log(filteredValue);
-console.log(filteredIndex);
+console.log(filteredValue); // [ 1, 2, 3 ]
+console.log(filteredIndex); // [ 2, 3 ]
