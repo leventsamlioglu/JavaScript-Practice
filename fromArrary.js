7a90279c6441391f843551a202ff068f6a5665ce
leventsamlioglu <leventsamlioglu@icloud.com>
2021-11-19 08:15:22 +0100

    code moodified

--- fromArrary.js
+++ fromArrary.js
@@ -44,12 +44,7 @@ console.log(Array.from({ length: 5 }, (v, i) => i));
 
 // Sequence generator function (commonly referred to as "range", e.g. Clojure, PHP etc)
 const range = (start, stop, step) =>
-  console.log(
-    Array.from(
-      { length: (stop - start) / step + 1 },
-      (_, i) => start + i * step
-    )
-  );
+  Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step);
 
 // Generate numbers range 0..4
 range(0, 4, 1);
@@ -60,7 +55,9 @@ range(1, 10, 2);
 // [1, 3, 5, 7, 9]
 
 // Generate the alphabet using Array.from making use of it being ordered as a sequence
-// let x= range("A".charCodeAt(0), "Z".charCodeAt(0), 1).map((x) =>
-//   String.fromCharCode(x)
-// );
+console.log(
+  range("A".charCodeAt(0), "Z".charCodeAt(0), 1).map((x) =>
+    String.fromCharCode(x)
+  )
+);
 // ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
