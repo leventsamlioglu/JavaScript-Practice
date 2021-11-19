5f1873ce99ee3f2c9f122390195a9b782c541554
leventsamlioglu <leventsamlioglu@icloud.com>
2021-11-19 08:23:16 +0100

    code modified

--- iteratingAnArray.js
+++ iteratingAnArray.js
@@ -1,9 +1,9 @@
 const numbers = ["x", "y", "z"];
 
-for (let number of numbers) console.log(number);
-
-numbers.forEach(function (number, index, array) {
-  console.log(index, number, array);
-});
+for (let number of numbers) console.log(number); // x y z
 
 numbers.forEach((number, index, array) => console.log(index, number, array));
+
+// 0 x [ 'x', 'y', 'z' ]
+// 1 y [ 'x', 'y', 'z' ]
+// 2 z [ 'x', 'y', 'z' ]
