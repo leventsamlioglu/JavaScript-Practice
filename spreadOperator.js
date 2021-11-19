cfc71c997d9c39bb82e7919a5038bddc30ed95ac
leventsamlioglu <leventsamlioglu@icloud.com>
2021-11-19 09:26:05 +0100

    add output comment

--- spreadOperator.js
+++ spreadOperator.js
@@ -4,5 +4,5 @@ const second = [5, 6, 7, 8];
 const combined = ["a", ...first, "b", ...second];
 const copy = [...combined];
 
-console.log(combined);
-console.log(copy);
+console.log(combined); // [ 'a', 1, 2, 3, 4, 'b', 5, 6, 7, 8 ]
+console.log(copy === combined); // false
