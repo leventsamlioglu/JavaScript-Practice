4ddbd2347977537f1fe9c004d5161d1b6382e633
leventsamlioglu <leventsamlioglu@icloud.com>
2021-11-27 15:51:15 +0100

    call&bind method

--- callMethod2.js
+++ callMethod2.js
@@ -1,13 +1,28 @@
 const person = {
-  fullName: function(city, country) {
+  fullName: function (city, country) {
     return this.firstName + " " + this.lastName + "," + city + "," + country;
-  }
-}
+  },
+};
 
 const person1 = {
-  firstName:"John",
-  lastName: "Doe"
-}
+  firstName: "John",
+  lastName: "Doe",
+};
 
 // This will return John Doe,Oslo,Norway
 console.log(person.fullName.call(person1, "Oslo", "Norway"));
+
+//-------------------------------------------------------------------------------
+
+const calc = {
+  multiply: function (a, b) {
+    return a * b;
+  },
+  multiplyMany: function (...args) {
+    return [].reduce.call(args, this.multiply);
+  },
+};
+
+const multiplyManyBySix = calc.multiplyMany.bind(calc, 1, 2, 3);
+
+console.log(multiplyManyBySix(4, 5, 10, 10)); // returns 12000
