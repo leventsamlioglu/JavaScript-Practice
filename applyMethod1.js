5d31ef1dc763da046373c4aedc7113c34856b510
leventsamlioglu <leventsamlioglu@icloud.com>
2021-11-29 09:34:10 +0100

    apply method

--- applyMethod1.js
+++ applyMethod1.js
@@ -12,3 +12,19 @@ const person1 = {
 console.log(person.fullName.apply(person1, ["Oslo", "Norway"]));
 
 // Outputs: John Doe,Oslo,Norway
+
+//----------------------------------------------------------------------------------------------
+
+const array = ["a", "b"];
+const elements = [0, 1, 2];
+array.push.apply(array, elements);
+console.info(array); // ["a", "b", 0, 1, 2]
+
+//----------------------------------------------------------------------------------------------
+
+const numbers = [5, 6, 2, 3, 7];
+
+let max = Math.max.apply(null, numbers);
+// This about equal to Math.max(numbers[0], ...)
+// or Math.max(5, 6, ...)
+let min = Math.min.apply(null, numbers);
