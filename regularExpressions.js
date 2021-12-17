04615308267fbb3cb3a0ceec38c3ad2e066c9c15
leventsamlioglu <leventsamlioglu@icloud.com>
2021-12-17 23:17:08 +0100

    add output comment

--- regularExpressions.js
+++ regularExpressions.js
@@ -1,21 +1,11 @@
-console.log(randomIntBetween(1, 10));
-
-function productDescription(strings, productName, productPrice) {
+function productDescription(strings) {
   console.log(strings);
-  console.log(productName);
-  console.log(productPrice);
-  let priceCategory = "pretty cheap regarding its price";
-  if (productPrice > 20) {
-    priceCategory = "fairly priced";
-  }
-  // return `${strings[0]}${productName}${strings[1]}${priceCategory}${
-  //   strings[2]
-  // }`;
-  return { name: productName, price: productPrice };
+  // This product (JavaScript Course) is 29.99.
 }
 
 const prodName = "JavaScript Course";
 const prodPrice = 29.99;
 
-const productOutput = productDescription`This product (${prodName}) is ${prodPrice}.`;
-console.log(productOutput);
+const productOutput = productDescription(
+  `This product (${prodName}) is ${prodPrice}.`
+);
